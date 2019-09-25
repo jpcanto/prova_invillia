f72c7d3a9d9722555bf00ea20f95de801518db54
jpcanto <jplopessk8@gmail.com>
2019-09-25 00:07:13 -0300

    component form structure

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import Form from './components/form/form';
 
 export default class Main extends Component {
   render() {
     return (
       <section className="main-content">
-
+        <Form />
       </section>
     );
   }
