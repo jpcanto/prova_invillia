1b1c3d183d159f16bde3a95a91d6a754558444fe
jpcanto <jplopessk8@gmail.com>
2019-09-25 09:45:03 -0300

    Iniciando dinamismo na busca pela api

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
 import Form from './components/form/form';
+import * as StarWars from './services/starWars.js';
 
 export default class Main extends Component {
+
+  state = {
+    module: '',
+    numeracao: 1
+  };
+
+  async getStarWarsData() {
+    let data = await StarWars.StarWarsApi(this.state.module, this.state.numeracao);
+    this.setState({
+      starWars: data.results
+    })
+  }
+
+  componentWillMount() {
+    this.getStarWarsData();
+  }
+
   render() {
     return (
       <section className="main-content">
-        <Form />
+        <Form state={this.state} />
       </section>
     );
   }
--- src/components/form/form.jsx
+++ src/components/form/form.jsx
@@ -2,14 +2,22 @@ import React, { Component } from 'react';
 import Preview from './../../components/preview/preview';
 
 export default class form extends Component {
+
     render() {
         return (
             <aside>
                 <div className="form">
-                    <div className="form__name-box">
-                        <input type="text" placeholder="Nome do personagem" />
+                    <div className="form__select-box">
+                        <select onChange={(e) => this.setState({ modulo: e.target.value })}>
+                            <option value="personagem">Personagem</option>
+                            <option value="planeta">Planetas</option>
+                            <option value="nave">Naves</option>
+                            <option value="filme">Filmes</option>
+                        </select>
                     </div>
+                    <button className="form__pesquisar">Pesquisar</button>
                 </div>
+                <Preview />
             </aside>
         );
     }
